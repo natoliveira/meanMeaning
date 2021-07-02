277947402a7d6af3cd96924664009fcfb84fbc4c
natoliveira <nataliadeolive@gmail.com>
2021-07-01 21:07:45 -0300

    implementacoes

---
+++ cmake-build-debug/CMakeFiles/untitled35.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled35.dir/meediaAritmetica.cpp.o"
+  "untitled35"
+  "untitled35.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled35.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
